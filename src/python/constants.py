85be54fdf37e96833a473ac2004b94d60e12a8dc
Shalin Shekhar Mangar <shalin@apache.org>
2018-05-22 11:20:54 +0530

    Restore test environment defaults

--- src/python/constants.py
+++ src/python/constants.py
@@ -15,17 +15,17 @@ NIGHTLY_REPORTS_DIR = '%s/reports' % DATA_BASE_DIR
 SOLR_COLLECTION_NAME = 'gettingstarted'
 
 IMDB_DATA_FILE = '%s/imdb.json' % DATA_BASE_DIR
-# IMDB_NUM_DOCS = 2436442
-IMDB_NUM_DOCS = 0
+IMDB_NUM_DOCS = 2436442
+# IMDB_NUM_DOCS = 0
 
-# WIKI_1K_DATA_FILE = '%s/enwiki-20120502-lines-1k.txt' % DATA_BASE_DIR
-# WIKI_1K_NUM_DOCS = 33332620
-WIKI_1K_DATA_FILE = '%s/small-enwiki-20120502-lines-1k.txt' % DATA_BASE_DIR
-WIKI_1K_NUM_DOCS = 9
+WIKI_1K_DATA_FILE = '%s/enwiki-20120502-lines-1k.txt' % DATA_BASE_DIR
+WIKI_1K_NUM_DOCS = 33332620
+# WIKI_1K_DATA_FILE = '%s/small-enwiki-20120502-lines-1k.txt' % DATA_BASE_DIR
+# WIKI_1K_NUM_DOCS = 9
 # WIKI_1K_DATA_FILE = '%s/enwiki.random.lines.txt' % DATA_BASE_DIR
 
-#WIKI_4K_DATA_FILE = '%s/enwiki-20120502-lines.txt' % DATA_BASE_DIR
-#WIKI_4k_NUM_DOCS = 6726515
+WIKI_4K_DATA_FILE = '%s/enwiki-20120502-lines.txt' % DATA_BASE_DIR
+WIKI_4k_NUM_DOCS = 6726515
 # WIKI_4K_DATA_FILE = '%s/enwiki.random.lines.txt' % DATA_BASE_DIR
 # WIKI_4K_DATA_FILE = '%s/small-4k-wiki-lines.txt' % DATA_BASE_DIR
 # WIKI_4k_NUM_DOCS = 999
