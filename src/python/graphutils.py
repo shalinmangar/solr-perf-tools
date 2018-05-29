e77b68eb4f90c13198a53c9b8b053ddf700d6c86
Shalin Shekhar Mangar <shalin@apache.org>
2018-05-29 22:15:05 +0530

    Adjust height/width of graphs to have enough space for tooltips and annotations
    (cherry picked from commit f6ddf02)

--- src/python/graphutils.py
+++ src/python/graphutils.py
@@ -30,7 +30,7 @@ def getOneGraphHTML(annotations, id, data, yLabel, title, errorBars=True):
     l = []
     w = l.append
     series = data[0].split(',')[1]
-    w('<div id="%s" style="height:70%%; width: 90%%"></div>' % id)
+    w('<div id="%s" style="height:50%%; width: 80%%"></div>' % id)
     w('<script type="text/javascript">')
     w(onClickJS)
     w('  g_%s = new Dygraph(' % id)
